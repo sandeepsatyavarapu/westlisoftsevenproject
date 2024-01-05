2c60f471f82e5e274f83cedfc59d410536d8c9fa
sandeepsatyavarapu <sandeepsatyavarapu462@gmail.com>
2024-01-05 08:05:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function add()
+{
+    var name=document.getElementById("input1").value;
+    document.getElementById("label1").textContent=name;
+    var name=document.getElementById("input3").value;
+    document.getElementById("label2").textContent=name;
+}
+function add1()
+{
+    var name=document.getElementById("input2").value;
+    document.getElementById("labe3").textContent=name;
+    var name=document.getElementById("input4").value;
+    document.getElementById("label4").textContent=name;
+    document.getElementById("Form1").reset();
+}
